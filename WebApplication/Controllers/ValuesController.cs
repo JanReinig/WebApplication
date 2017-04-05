e57cbb5f9327aaaec7e78d65267d5d07b8b805d8
Jan Reinig <Jan Reinig>
2017-04-05 16:16:22 +0200

    First commit

---
+++ WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,97 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication.Controllers
+{
+    public class Person
+    {
+        public string Salutation { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string City { get; set; }
+
+        [BsonId]
+        public ObjectId _id { get; set; }
+    }
+
+    public class ValuesController : ApiController
+    {
+        private MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+        [Route("api/persons")]
+        [HttpGet]
+        public IList<Person> GetPersons()
+        {
+            IMongoDatabase database = client.GetDatabase("test");
+            IMongoCollection<Person> collection = database.GetCollection<Person>("persons");
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        [Route("api/personsByFirstName")]
+        [HttpGet]
+        public IList<Person> GetPersonsByFirstName(string firstName)
+        {
+            IMongoDatabase database = client.GetDatabase("test");
+            IMongoCollection<Person> collection = database.GetCollection<Person>("persons");
+            var query = Builders<Person>.Filter.Eq("FirstName", firstName);
+            return collection.Find(query).ToList();
+        }
+
+        [Route("api/personCount")]
+        [HttpGet]
+        public long GetPersonCount()
+        {
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<Person>("persons");
+            return collection.Count(new BsonDocument());
+        }
+
+        [Route("api/person/{id}")]
+        [HttpGet]
+        public Person GetPerson(string id)
+        {
+            ObjectId id2 = new ObjectId(id);
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<Person>("persons");
+            var query = Builders<Person>.Filter.Eq("_id", id2);
+            return collection.Find(query).FirstOrDefault();
+        }
+
+        [Route("api/person")]
+        [HttpPost]
+        public void PutPerson(Person person)
+        {
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<Person>("persons");
+            collection.InsertOne(person);
+        }
+
+        [Route("api/person/{id}")]
+        [HttpDelete]
+        public void Delete(string id)
+        {
+            ObjectId id2 = new ObjectId(id);
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<Person>("persons");
+            var query = Builders<Person>.Filter.Eq("_id", id2);
+            collection.DeleteOne(query);
+        }
+
+        [Route("api/persons")]
+        [HttpDelete]
+        public void Delete()
+        {
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<Person>("persons");
+            collection.DeleteMany(new BsonDocument());
+        }
+    }
+}
---
+++ WebApplication/script.js
@@ -0,0 +1,114 @@
+﻿function success() {
+    console.log("success");
+    setPersonCount();
+    generatePersonList();
+}
+
+function onLoad() {
+    console.log("onLoad entered");
+
+    registerNewPersonButton();
+    registerDeletePersonButton();
+    registerUpdateCount();
+    registerSelectSalutation();
+    setPersonCount();
+    generatePersonList();
+
+    console.log("onLoad left");
+}
+
+function successSetPersonCount(data, status, xhr) {
+    console.log(data);
+    $("#personCount").text(data);
+}
+
+function setPersonCount() {
+    $.ajax({
+        type: "GET",
+        url: "/api/personCount",
+        success: successSetPersonCount
+    });
+}
+
+function successGeneratePersonList(data, status, xhr) {
+    console.log(data);
+    data.forEach(function (person, index, list) {
+        console.log(person);
+        $("#personList").append("<a href='#' class='list-group-item'><h4 class='list-group-item-heading'>" + person.FirstName + " " + person.LastName +
+            "</h4><p class='list-group-item-text'>" + person.Salutation + "," + " " + person.City + "," + " " + person.DateOfBirth +
+    "</p></a>");
+    });
+}
+
+function generatePersonList() {
+    $("#personList").empty();
+    $.ajax({
+        type: "GET",
+        url: "/api/persons",
+        success: successGeneratePersonList
+    });
+}
+
+function registerNewPersonButton() {
+    $("#button").click(function () {
+        console.log("The button was clicked.");
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+        var dateOfBirth = $("#dateOfBirth").val();
+        var salutation = $("#salutation").val();
+        var city = $("#city").val();
+        console.log("First name: " + firstName);
+        console.log("Last name: " + lastName);
+        console.log("Date of birth: " + dateOfBirth);
+        console.log("City: " + city);
+        console.log("Salutation: " + salutation);
+        $.ajax({
+            type: "POST",
+            url: "/api/person",
+            data: {
+                FirstName: firstName,
+                LastName: lastName,
+                DateOfBirth: dateOfBirth,
+                City: city,
+                salutation: salutation,
+
+            },
+            success: success
+        });
+    });
+}
+
+function registerSelectSalutation() {
+    console.log("registerSelectSalutation");
+    $("#salutationMr").click(function () {
+        $("#salutation").text("Salutation: Mr.").append(" <span class='caret'></span>");
+    });
+    $("#salutationMrs").click(function () {
+        $("#salutation").text("Salutation: Mrs.").append(" <span class='caret'></span>");
+    });
+}
+
+function selectSalutation(salutation) {
+    $("#salutation").text(saluation);
+}
+
+function registerUpdateCount() {
+    $("#updateCount").click(function () {
+        setPersonCount();
+    });
+}
+
+function registerDeletePersonButton() {
+    $("#buttonDelete").click(function () {
+        console.log("Delete button has been pressed");
+        $.ajax({
+            type: "DELETE",
+            url: "/api/persons",
+            success: function () {
+                console.log("Delete has been successful");
+                setPersonCount();
+                generatePersonList();
+            }
+        });
+    });    
+}
